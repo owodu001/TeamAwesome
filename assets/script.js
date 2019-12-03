e8d2024e0749d65f83fc93b376d2bcbbf2778282
Paul Chheang <phalcheat@gmail.com>
2019-12-02 20:45:56 -0600

    Updated function displayresto() to show only restaurants that have picture

--- assets/script.js
+++ assets/script.js
@@ -91,20 +91,23 @@ function displayresto() {
             console.log(typeof restoObj);
             console.log("Resto data: ", restoObj);
             $.each(restoObj, function (index, value) {
-                let location = restoObj.restaurant.location;
-                let userRating = restoObj.restaurant.user_rating;
-                html += "<div class='data img-rounded'>";
-                html += "<div class='rating'>";
-                html += "<span title='" + userRating.rating_text + "'><p style='color:white;background-color:#" + userRating.rating_color + ";border-radius:4px;border:none;padding:2px 10px 2px 10px;text-align: center;text-decoration:none;display:inline-block;font-size:16px;float:right;'><strong>" + userRating.aggregate_rating + "</strong></p></span><br>";
-                html += "  <strong class='text-info'>" + userRating.votes + " votes</strong>";
-                html += "</div>";
-                html += "<img class='resimg img-rounded' src=" + value.thumb + " alt='Restaurant Image' height='185' width='185'>";
-                html += "<a href=" + value.url + " target='_blank' class='action_link'><h2 style='color:red;'><strong>" + value.name + "</strong></h2></a>";
-                html += "  <strong class='text-primary'>" + location.locality + "</strong><br>";
-                html += "  <h6 style='color:grey;'><strong>" + location.address + "</strong></h6><hr>";
-                html += "  <strong>CUISINES</strong>: " + value.cuisines + "<br>";
-                html += "  <strong>COST FOR TWO</strong>: " + value.currency + value.average_cost_for_two + "<br>";
-                html += "</div><br>";
+                if (value.thumb != ""){
+                    let location = restoObj.restaurant.location;
+                    let userRating = restoObj.restaurant.user_rating;
+                    html += "<div class='data img-rounded'>";
+                    html += "<div class='rating'>";
+                    html += "<span title='" + userRating.rating_text + "'><p style='color:white;background-color:#" + userRating.rating_color + ";border-radius:4px;border:none;padding:2px 10px 2px 10px;text-align: center;text-decoration:none;display:inline-block;font-size:16px;float:right;'><strong>" + userRating.aggregate_rating + "</strong></p></span><br>";
+                    html += "  <strong class='text-info'>" + userRating.votes + " votes</strong>";
+                    html += "</div>";
+                    html += "<img class='resimg img-rounded' src=" + value.thumb + " alt='Restaurant Image' height='185' width='185'>";
+                    html += "<a href=" + value.url + " target='_blank' class='action_link'><h2 style='color:red;'><strong>" + value.name + "</strong></h2></a>";
+                    html += "  <strong class='text-primary'>" + location.locality + "</strong><br>";
+                    html += "  <h6 style='color:grey;'><strong>" + location.address + "</strong></h6><hr>";
+                    html += "  <strong>CUISINES</strong>: " + value.cuisines + "<br>";
+                    html += "  <strong>COST FOR TWO</strong>: " + value.currency + value.average_cost_for_two + "<br>";
+                    html += "</div><br>";
+                }
+                
             });
         });
         $("#food-info").html(html);
